54da89d5833635c03e971eddd979cac2907a1afe
Zein Reyes <zein.reyes2005@gmail.com>
2023-11-22 18:26:16 +0800

    edited menu page

--- index.js
+++ index.js
@@ -1,4 +1,58 @@
-function changeImage(fileName) {
-let img = document.querySelector("#banner");
-img.setAttribute("src", fileName);
-}
+document.addEventListener('DOMContentLoaded', function () {
+    let pizzaNav = document.getElementById('pizza-nav');
+    let pastaNav = document.getElementById('pasta-nav');
+    let sidesNav = document.getElementById('side-nav');
+    let drinkNav = document.getElementById('drink-nav');
+  
+    let pizzaContainer = document.getElementById('pizza-container');
+    let pastaContainer = document.getElementById('pasta-container');
+    let sidesContainer = document.getElementById('sides-container');
+    let drinkContainer = document.getElementById('drinks-container');
+  
+    function hideAllContainers() {
+      pizzaContainer.classList.remove('active');
+      pastaContainer.classList.remove('active');
+      sidesContainer.classList.remove('active');
+      drinkContainer.classList.remove('active');
+    }
+  
+    // Set the initial state
+    pizzaContainer.classList.add('active');
+    pizzaNav.classList.add('active');
+  
+    pizzaNav.addEventListener('click', function () {
+      hideAllContainers();
+      pizzaContainer.classList.add('active');
+      pizzaNav.classList.add('active');
+    });
+  
+    pastaNav.addEventListener('click', function () {
+      hideAllContainers();
+      pastaContainer.classList.add('active');
+      pastaNav.classList.add('active');
+    });
+  
+    sidesNav.addEventListener('click', function () {
+      hideAllContainers();
+      sidesContainer.classList.add('active');
+      sidesNav.classList.add('active');
+    });
+  
+    drinkNav.addEventListener('click', function () {
+      hideAllContainers();
+      drinkContainer.classList.add('active');
+      drinkNav.classList.add('active');
+    });
+    
+    // Scroll to the clicked section smoothly
+    document.querySelectorAll('.menu-nav a').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+  
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+  });
+  
